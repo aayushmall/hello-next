ac2967e961fc9d6423cc51fb1901be319c54d42d
Aayush Mall <mall.aayush99@gmail.com>
2019-07-16 19:44:00 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// This is the Link API
+import Link from 'next/link';
+import Header from '../comps/header';
+
+const Index = () => (
+  <div>
+    <Header />
+    <p>Hello Next.js</p>
+  </div>
+);
+
+export default Index;
